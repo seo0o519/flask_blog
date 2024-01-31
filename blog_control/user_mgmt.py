31989d5c857f7c7b06831536b3d1f99ab08add18
seo0o519 <seoyoung344@naver.com>
2024-01-31 19:15:58 +0900

    feat: add sign in

--- blog_control/user_mgmt.py
+++ blog_control/user_mgmt.py
@@ -2,10 +2,47 @@ from flask_login import UserMixin
 from blog_data.mysql import conn_mysqldb
 
 class User(UserMixin): # UserMixin class 상속받음
-    def __init__(self, user_id, user_email, user_pw):
+    def __init__(self, user_id, user_email, user_passwd):
         self.id = user_id # id는 flask_login 코드 안에서도 access하기 때문에 이름 바꾸면 안됨
         self.user_email = user_email
-        self.user_pw = user_pw
+        self.user_passwd = user_passwd
 
     def get_id(self):
         return str(self.id)
+    
+    @staticmethod
+    def get(user_id):
+        mysql_db = conn_mysqldb()
+        db_cursor = mysql_db.cursor()
+        sql = "SELECT * FROM user_infomation WHERE USER_ID = '" + str(user_id) + "'"
+        user = db_cursor.fetchone()
+        if not user:
+            return None
+        
+        user = User(user_id=user[0], user_email=user[1], user_passwd=user[2])
+        return user
+    
+    @staticmethod
+    def find(user_email):
+        mysql_db = conn_mysqldb()
+        db_cursor = mysql_db.cursor()
+        sql = "SELECT * FROM user_information WHERE USER_EMAIL = '" + str(user_email) + "'"
+        db_cursor.execute(sql)
+        user = db_cursor.fetchone()
+        if not user:
+            return None
+        user = User(user_id=user[0], user_email=user[1], user_passwd=user[2])
+        return user
+    
+    @staticmethod
+    def create(user_email, user_passwd):
+        user = User.find(user_email)
+        if user==None:
+            mysql_db = conn_mysqldb()
+            db_cursor = mysql_db.cursor()
+            sql = "INSERT INTO user_information (USER_EMAIL, USER_PASSWD) VALUES ('%s', '%s')" % (str(user_email), str(user_passwd))
+            db_cursor.execute(sql)
+            mysql_db.commit()
+            return User.find(user_email)
+        else:
+            return user
--- blog_view/blog.py
+++ blog_view/blog.py
@@ -1,6 +1,14 @@
-from flask import Flask, jsonify, request, render_template, make_response, Blueprint
+from flask import Flask, jsonify, request, render_template, make_response, Blueprint, redirect, url_for
+from blog_control.user_mgmt import User
 
 blog = Blueprint('blog', __name__)
+
+@blog.route('set_info', methods=['GET', 'POST'])
+def set_info():
+    if request.method == 'POST':
+        print("HHHHHHHHHHHHHHHHHHHHHH",request.form)
+        user = User.create(request.form['user_email'], request.form['user_passwd'])
+        return redirect(url_for('blog.home'))
 
 @blog.route('/home')
 def home():
